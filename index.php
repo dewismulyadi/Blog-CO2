68f3758dd2ef86bbb599a1efb345e470c1273c08
rasya <rasyahroel132@gmail.com>
2021-10-06 15:19:28 +0700

    Finishing Landing Page

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         </div>
      
         <div class="row">
-            <div class="col-12 text-center py-5">
+            <div class="col-12 text-center py-4">
                 <h1>Welcome to Collection of Latest</h1>
                 <h1 class="text-yellow">Articles and News</h1>
                 <h4><strong>“Updated and Reliable”</strong></h4>
@@ -23,9 +23,7 @@
 
         <div class="row row-1 bg-yellowalt rounded rounded-alt text-dark text-center p-5">
             <div class="col-md-12">
-                <div class="d-flex flex-column ">
-                    <p>We are here to collect news and articles from all national, international and trusted sources and are always updated in gathering news.<br><br>We accept submissions in the form of articles if the articles we receive are appropriate and appropriate to be posted on our website.</p>
-                </div>
+                <p>We are here to collect news and articles from all national, international and trusted sources and are always updated in gathering news.<br><br>We accept submissions in the form of articles if the articles we receive are appropriate and appropriate to be posted on our website.</p>
             </div>
             <div class="col-md-12 mt-3">
                 <button class="bg-darkalt rounded rounded-alt text-yellow p-2 mx-5">
@@ -34,6 +32,83 @@
                 <button class="bg-darkalt rounded rounded-alt text-yellow p-2 mx-5">
                     Contact Us
                 </button>
+            </div>
+        </div>
+        <p class="text-center">For any other inquiries please contact <a href="#">support@codingcollective.com</a></p>
+
+        <div class="row">
+            <div class="col-12 text-center py-4">
+                <h2><strong>Articles</strong></h2>
+                <h4>Most Read :</h4>
+            </div>
+        </div>
+
+        <div class="row row-cols-md-3">
+            <div class="col">
+                <div class="card bg-darkalt text-white rounded rounded-alt">
+                    <img src="img/mie.jpg" alt="Mie Instan" class="card-img-top">
+                    <div class="card-body text-center">
+                        <a href="#" class="text-white"><h5 class="card-title">Dampak Buruk Mie Instan</h5></a>
+                        <p class="card-text">Mie instan adalah makanan yang rasannya sangat enak. Tak heran bila makanan in banyak dicintai oleh masyarakat. Namun ternyata sering mengkonsumsi mie instan membawa dampak buruk bagi tubuh. Berdasarkan....</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card bg-darkalt text-white rounded rounded-alt">
+                    <img src="img/mie.jpg" alt="Mie Instan" class="card-img-top">
+                    <div class="card-body text-center">
+                        <a href="#" class="text-white"><h5 class="card-title">Berhasilkah Pendidikan Kita?</h5></a>
+                        <p class="card-text">Pendidikan merupakan sebuah usaha atau proses yang melibatkan peserta didik dan pengajar dalam suatu waktu dengan tujuan mencapai manusia yang bermartabat. Di Indonesia....</p>
+                    </div>
+                </div>
+            </div>
+                
+            <div class="col">
+                <div class="card bg-darkalt text-white rounded rounded-alt">
+                    <img src="img/mie.jpg" alt="Mie Instan" class="card-img-top">
+                    <div class="card-body text-center">
+                        <a href="#" class="text-white"><h5 class="card-title">Teknologi GPS Terbaru</h5></a>
+                        <p class="card-text">Informasi tebaru mengatakan bahwa perusahaan X telah berhasil meluncurkan produk baru dengan teknologi GPS yang sangat canggih. Perusahaan X berhasil mengembangkan sebuah smartphone</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 text-center py-4">
+                <h2><strong>News</strong></h2>
+                <h4>Most Read :</h4>
+            </div>
+        </div>
+
+        <div class="row row-cols-md-3 mb-4">
+            <div class="col">
+                <div class="card bg-yellowalt text-dark rounded rounded-alt">
+                    <img src="img/mie.jpg" alt="Mie Instan" class="card-img-top">
+                    <div class="card-body text-center">
+                        <a href="#" class="text-dark"><h5 class="card-title">Dampak Buruk Mie Instan</h5></a>
+                        <p class="card-text">Mie instan adalah makanan yang rasannya sangat enak. Tak heran bila makanan in banyak dicintai oleh masyarakat. Namun ternyata sering mengkonsumsi mie instan membawa dampak buruk bagi tubuh. Berdasarkan....</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card bg-yellowalt text-dark rounded rounded-alt">
+                    <img src="img/mie.jpg" alt="Mie Instan" class="card-img-top">
+                    <div class="card-body text-center">
+                        <a href="#" class="text-dark"><h5 class="card-title">Berhasilkah Pendidikan Kita?</h5></a>
+                        <p class="card-text">Pendidikan merupakan sebuah usaha atau proses yang melibatkan peserta didik dan pengajar dalam suatu waktu dengan tujuan mencapai manusia yang bermartabat. Di Indonesia....</p>
+                    </div>
+                </div>
+            </div>
+                
+            <div class="col">
+                <div class="card bg-yellowalt text-dark rounded rounded-alt">
+                    <img src="img/mie.jpg" alt="Mie Instan" class="card-img-top">
+                    <div class="card-body text-center">
+                        <a href="#" class="text-dark"><h5 class="card-title">Teknologi GPS Terbaru</h5></a>
+                        <p class="card-text">Informasi tebaru mengatakan bahwa perusahaan X telah berhasil meluncurkan produk baru dengan teknologi GPS yang sangat canggih. Perusahaan X berhasil mengembangkan sebuah smartphone</p>
+                    </div>
+                </div>
             </div>
         </div>
 
