ba5a4928a4d8ee22d35db84bc0b32dacc8b6e2ab
rasya <rasyahroel132@gmail.com>
2021-10-08 14:53:13 +0700

    Revisi Articles

--- article.php
+++ article.php
@@ -1,171 +1,72 @@
 <?php
 	include 'header.php';
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-    <!-- MDB -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet" />
-    <title>Articles</title>
-    <style>
-        body {
-            background: hsl(0, 0%, 90%);
-            font-family: Roboto, sans-serif;
 
-        }
-        h1{
-            text-align: center;
-            margin-top: 50px;
-            color: #474443;
-        }
-        .middle{
-            text-align: center;
-            color: #474443;
-        }
-        .list-update{
-            margin-left: 60px;
-            color: #474443;
-        }
-        .list-article{
-            margin: 20px;
-            display: flex;
-            color: #474443;
-        }
-        .list-article ul {
-            list-style-type: none;
-            width: 500px;
-        }
- 
-        .list-article h3 {
-            font: bold 20px;
-        }
- 
-        .list-article img {
-            float: left;
-            margin: 0 15px 0 0;
-            height: 143px;
-            width: 333px;
-        }
- 
-        .list-article p {
-            font: 200 12px;
-        }
-         
-        .list-article li {
-            padding: 10px;
-            overflow: auto;
-            width: 1216px;
-        }
-         
-        .list-article li:hover {
-            background: #474443;
-            color: #FFFFFF;
-            cursor: pointer;
-        }
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 text-center py-4">
+                <a href="#" class="text-dark text-decoration-none"><h2><strong>Articles</strong></h2></a>
+                <h4>Most Read :</h4>
+            </div>
+        </div>
 
-        .article-card img {
-            display: block;
-            border: 0;
-            width: 100%;
-            height: 269px;
-            border-top-right-radius: 58px;
-            border-top-left-radius: 58px;
-        }
-        .article-card {
-            background: #474443;
-            margin: 50px;
-            width: 403px;
-            height: 570px;
-            border-radius: 58px;
-            color: #FFFFFF;
-            text-align: center;
-        }
+        <div class="row text-center justify-content-center mx-auto mb-5">
+            <div class="col-md-3 col-sm-12 bg-darkalt text-white p-0 mx-auto rounded-alt mb-4">
+                <img src="img/mie.jpg" alt="Mie Instan" class="img-fluid rounded-top-alt">
+                <div class="p-3">
+                    <a href="#" class="text-white text-decoration-none"><h4 class="card-title"><strong>Dampak Buruk Mie Instan</strong></h4></a>
+                    <p class="card-text p-3">Mie instan adalah makanan yang rasannya sangat enak. Tak heran bila makanan in banyak dicintai oleh masyarakat. Namun ternyata sering mengkonsumsi mie instan membawa dampak buruk bagi tubuh. Berdasarkan....</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-12 bg-darkalt text-white p-0 mx-auto rounded-alt mb-4">
+                <img src="img/pendidikan.jpg" alt="Pendidikan" class="img-fluid rounded-top-alt">
+                <div class="p-3">
+                    <a href="#" class="text-white text-decoration-none"><h4 class="card-title"><strong>Berhasilkah Pendidikan Kita?</strong></h4></a>
+                    <p class="card-text p-3">Pendidikan merupakan sebuah usaha atau proses yang melibatkan peserta didik dan pengajar dalam suatu waktu dengan tujuan mencapai manusia yang bermartabat. Di Indonesia....</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-12 bg-darkalt text-white p-0 mx-auto rounded-alt mb-4">
+                <img src="img/gps.jpg" alt="Teknologi GPS" class="img-fluid rounded-top-alt">
+                <div class="p-3">
+                    <a href="#" class="text-white text-decoration-none"><h4 class="card-title"><strong>Teknologi GPS Terbaru</strong></h4></a>
+                    <p class="card-text p-3">Informasi tebaru mengatakan bahwa perusahaan X telah berhasil meluncurkan produk baru dengan teknologi GPS yang sangat canggih. Perusahaan X berhasil mengembangkan sebuah smartphone</p>
+                </div>
+            </div>
+        </div>
 
-        .article-card a {
-            color: #FFFFFF;
-            text-decoration: none;
-        }
+        <div class="row ms-5 p-3">
+            <h2>Day, dd Month yyyy</h2>
+        </div>
 
-        .article-card a:hover {
-            box-shadow: 3px 3px 8px hsl(0, 0%, 80%);
-        }
-
-        .article-card-content {
-            padding: 1.4em;
-        }
-
-        .article-card-content h2 {
-            margin-top: 0;
-            margin-bottom: .5em;
-            font-weight: bold;
-            
-        }
-
-        .article-card-content p {
-            font-size: 80%;
-        }
-
-        /* Flexbox stuff */
-
-        .article-cards {
-           display: flex;
-           flex-wrap: wrap;
-           justify-content: space-between;
-        }
-    </style>
-</head>
-<body>
-    <h1>Articles</h1>
-    <h2 class="middle">Most Read :</h2>
-    <section class="article-cards">
-        <article class="article-card">
-            <img src="img/mu.jpg" alt="Manchester United">
-            <div class="article-card-content">
-                <a href="#"><h2>Pulang ke Manchester United, Cristiano Ronaldo Pakai Nomor Punggung Berapa?</h2></a>
-                <p>Suara.com - Cristiano Ronaldo resmi kembali ke Manchester United, Jumat (27/8/2021) malam WIB. Lalu penyerang 36 tahun itu akan mengenakan nomor punggung berapa dalam periode keduanya merumput di Old Trafford? .....</p>
-            </div><!-- .card-content -->
-        </article><!-- .card -->
-
-        <article class="article-card">
-            <img src="img/positivity-rate.jpg" alt="Positivity Rate">
-            <div class="article-card-content">
-                <a href="#"><h2>468 Kasus Baru Covid-19 di Jakarta Hari Ini, Positivity Rate di Bawah Batas Aman WHO</h2></a>
-                <p>JAKARTA, KOMPAS.com - Pemprov DKI Jakarta kembali mengumumkan perkembangan terbaru kasus Covid-19 di Ibukota pada Sabtu (28/8/2021). Berikut rinciannya: ...</p>
-            </div><!-- .card-content -->
-        </article><!-- .card -->
-
-        <article class="article-card">
-            <img src="img/blood-vaksin.jpg" alt="Vaksin Covid-19">
-            <div class="article-card-content">
-                <a href="#"><h2>Beruntung Bagi yang Sudah Vaksin Covid-19, Penelitian Ini Ungkap Perbedaan Kondisi Orang Sebelum dan Sesudah Vaksin Jik Terinfeksi Covid-19, Ternyata Begini Kondisi Darahnya <br>....</h2></a>
-            </div><!-- .card-content -->
-        </article><!-- .card -->
-    </section><!-- .cards -->
-
-    <h2 class="list-update">Minggu, 23 Agustus 2021</h2>
-    <div class="list-article">
-        <ul>
-            <li>
-                <img src="img/mu.jpg" alt="Manchester United">
-                <h3>Suara.com - Cristiano Ronaldo resmi kembali ke Manchester United, Jumat (27/8/2021) malam WIB. Lalu penyerang 36 tahun itu akan mengenakan nomor punggung berapa dalam periode keduanya merumput di Old Trafford? .....</h3>
-                <p>JAKARTA, KOMPAS.com - Pemprov DKI Jakarta kembali mengumumkan perkembangan terbaru kasus Covid-19 di Ibukota pada Sabtu (28/8/2021). Berikut rinciannya: ...</p>
-            </li>
-              
-            <li>
-                <img src="img/positivity-rate.jpg" alt="Positivity Rate">
-                <h3>468 Kasus Baru Covid-19 di Jakarta Hari Ini, Positivity Rate di Bawah Batas Aman WHO</h3>
-                <p>JAKARTA, KOMPAS.com - Pemprov DKI Jakarta kembali mengumumkan perkembangan terbaru kasus Covid-19 di Ibukota pada Sabtu (28/8/2021). Berikut rinciannya: ...</p>
-            </li>
-         
-            <li>
-                <img src="img/blood-vaksin.jpg" alt="Vaksin Covid-19">
-                <h3>Beruntung Bagi yang Sudah Vaksin Covid-19, Penelitian Ini Ungkap Perbedaan Kondisi Orang Sebelum dan Sesudah Vaksin Jik Terinfeksi Covid-19, Ternyata Begini Kondisi Darahnya ....</h3>
-            </li>
-          </ul>
+        <div class="pb-5">
+            <div class="row my-2 py-2 justify-content-center">
+                <div class="col-md-3 col-sm-12 col-4">
+                    <img src="img/mie.jpg" alt="Mie Instan" class="img-fluid">
+                </div>
+                <div class="col-8">
+                    <a href="#" class="text-dark text-decoration-none"><h4><strong>Dampak Buruk Mie Instan</strong></h4></a>
+                    <p>Mie instan adalah makanan yang rasannya sangat enak. Tak heran bila makanan in banyak dicintai oleh masyarakat. Namun ternyata sering mengkonsumsi mie instan membawa dampak buruk bagi tubuh. Berdasarkan....</p>
+                </div>
+            </div>
+            <div class="row my-2 py-2 justify-content-center">
+                <div class="col-md-3 col-sm-12 col-4">
+                    <img src="img/pendidikan.jpg" alt="Pendidikan" class="img-fluid">
+                </div>
+                <div class="col-8">
+                    <a href="#" class="text-dark text-decoration-none"><h4><strong>Berhasilkah Pendidikan Kita?</strong></h4></a>
+                    <p>Pendidikan merupakan sebuah usaha atau proses yang melibatkan peserta didik dan pengajar dalam suatu waktu dengan tujuan mencapai manusia yang bermartabat. Di Indonesia....</p>
+                </div>
+            </div>
+            <div class="row my-2 py-2 justify-content-center">
+                <div class="col-md-3 col-sm-12 col-4">
+                    <img src="img/gps.jpg" alt="Teknologi GPS" class="img-fluid">
+                </div>
+                <div class="col-8">
+                    <a href="#" class="text-dark text-decoration-none"><h4><strong>Teknologi GPS Terbaru</strong></h4></a>
+                    <p>Informasi tebaru mengatakan bahwa perusahaan X telah berhasil meluncurkan produk baru dengan teknologi GPS yang sangat canggih. Perusahaan X berhasil mengembangkan sebuah smartphone</p>
+                </div>
+            </div>
+        </div>
     </div>
-</body>
-</html>
+
+<?php include 'footer.php' ?>
