a16ec42b00df07ef01b672d6a1a847431731c457
rasya <rasyahroel132@gmail.com>
2021-10-08 13:54:16 +0700

    Revisi News

--- news.php
+++ news.php
@@ -1,61 +1,66 @@
 <?php include 'header.php' ?>
 
 <div class="container-fluid">
-    <div class="text-center mb-4">
-        <h3 >News</h3>
-    </div>
-    <div class="row text-center">
-        <div class="bg-yellowalt col p-0 ms-4 rounded-alt">
-            <img src="image/kasus.jpg" class="img-fluid rounded-top-alt">
-            <div>
-                <h3>Ini Judul</h3>
-                <p>Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk </p>
-            </div>
-        </div>
-        <div class="bg-yellowalt col p-0 ms-4 rounded rounded-alt">
-            <img src="image/kasus.jpg" class="img-fluid rounded-top-alt">
-            <div>
-                <h3>Ini Judul</h3>
-                <p>Potongan berita batas 30 kata</p>
-            </div>
-        </div>
-        <div class="bg-yellowalt col p-0 ms-4 me-4 rounded rounded-alt">
-            <img src="image/kasus.jpg" class="img-fluid rounded-top-alt">
-            <div>
-                <h3>Ini Judul</h3>
-                <p>Potongan berita batas 30 kata</p>
-            </div>
+    <div class="row">
+        <div class="col-12 text-center py-4">
+            <a href="#" class="text-dark text-decoration-none"><h2><strong>News</strong></h2></a>
         </div>
     </div>
-    <div class="m-4 p-4">
-        <h3>Day, dd Month yyyy</h3>
+
+    <div class="row text-center justify-content-center mx-auto mb-5">
+        <div class="col-md-3 col-sm-12 bg-yellowalt text-dark p-0 mx-auto rounded-alt mb-4">
+            <img src="img/mu.jpg" alt="Manchester United" class="img-fluid rounded-top-alt">
+            <div class="p-3">
+                <a href="#" class="text-dark text-decoration-none"><h4 class="card-title"><strong>Pulang ke Manchester United, Cristiano Ronaldo Pakai Nomor Punggung Berapa?</strong></h4></a>
+                <p class="card-text p-3">Suara.com - Cristiano Ronaldo resmi kembali ke Manchester United, Jumat (27/8/2021) malam WIB. Lalu penyerang 36 tahun itu akan mengenakan nomor punggung berapa dalam periode keduanya merumput di Old Trafford? .....</p>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-12 bg-yellowalt text-dark p-0 mx-auto rounded-alt mb-4">
+            <img src="img/positivity-rate.jpg" alt="Positivity Rate" class="img-fluid rounded-top-alt">
+            <div class="p-3">
+                <a href="#" class="text-dark text-decoration-none"><h4 class="card-title"><strong>468 Kasus Baru Covid-19 di Jakarta Hari Ini, Positivity Rate di Bawah Batas Aman WHO</strong></h4></a>
+                <p class="card-text p-3">JAKARTA, KOMPAS.com - Pemprov DKI Jakarta kembali mengumumkan perkembangan terbaru kasus Covid-19 di Ibukota pada Sabtu (28/8/2021). Berikut rinciannya:...</p>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-12 bg-yellowalt text-dark p-0 mx-auto rounded-alt mb-4">
+            <img src="img/blood-vaksin.jpg" alt="Vaksinasi" class="img-fluid rounded-top-alt">
+            <div class="p-3">
+                <a href="#" class="text-dark text-decoration-none"><h4 class="card-title"><strong>Beruntung Bagi yang Sudah Vaksin Covid-19, Penelitian Ini Ungkap Perbedaan Kondisi Orang Sebelum dan Sesudah Vaksin Jik Terinfeksi Covid-19, Ternyata Begini Kondisi Darahnya....</strong></h4></a>
+                <!-- <p class="card-text p-3">Informasi tebaru mengatakan bahwa perusahaan X telah berhasil meluncurkan produk baru dengan teknologi GPS yang sangat canggih. Perusahaan X berhasil mengembangkan sebuah smartphone</p> -->
+            </div>
+        </div>
     </div>
-    <div class="mx-4 px-4">
-        <div class="row my-2 py-2">
-            <div class="col-4">
-                <img src="image/kasus.jpg" class="img-fluid">
+
+    <div class="row ms-5 p-3">
+        <h2>Day, dd Month yyyy</h2>
+    </div>
+
+    <div class="pb-5">
+        <div class="row my-2 py-2 justify-content-center">
+            <div class="col-md-3 col-sm-12 col-4">
+                <img src="img/mu.jpg" alt="Manchester United" class="img-fluid">
             </div>
             <div class="col-8">
-                <h3>Ini Judul</h3>
-                <p>Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk </p>
+                <a href="#" class="text-dark text-decoration-none"><h4><strong>Pulang ke Manchester United, Cristiano Ronaldo Pakai Nomor Punggung Berapa?</strong></h4></a>
+                <p>Suara.com - Cristiano Ronaldo resmi kembali ke Manchester United, Jumat (27/8/2021) malam WIB. Lalu penyerang 36 tahun itu akan mengenakan nomor punggung berapa dalam periode keduanya merumput di Old Trafford? .....</p>
             </div>
         </div>
-        <div class="row my-2 py-2">
-            <div class="col-4">
-                <img src="image/kasus.jpg" class="img-fluid">
+        <div class="row my-2 py-2 justify-content-center">
+            <div class="col-md-3 col-sm-12 col-4">
+                <img src="img/positivity-rate.jpg" alt="Positivity Rate" class="img-fluid">
             </div>
             <div class="col-8">
-                <h3>Ini Judul</h3>
-                <p>Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk </p>
+                <a href="#" class="text-dark text-decoration-none"><h4><strong>468 Kasus Baru Covid-19 di Jakarta Hari Ini, Positivity Rate di Bawah Batas Aman WHO</strong></h4></a>
+                <p>JAKARTA, KOMPAS.com - Pemprov DKI Jakarta kembali mengumumkan perkembangan terbaru kasus Covid-19 di Ibukota pada Sabtu (28/8/2021). Berikut rinciannya:...</p>
             </div>
         </div>
-        <div class="row my-2 py-2">
-            <div class="col-4">
-                <img src="image/kasus.jpg" class="img-fluid">
+        <div class="row my-2 py-2 justify-content-center">
+            <div class="col-md-3 col-sm-12 col-4">
+                <img src="img/blood-vaksin.jpg" alt="Vaksinasi" class="img-fluid">
             </div>
             <div class="col-8">
-                <h3>Ini Judul</h3>
-                <p>Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk Potongan berita batas 30 katadfff ggggggggggg gggggggjfffff ffffffffffffffff fffffffffffffffff fffffffffffhgk </p>
+                <a href="#" class="text-dark text-decoration-none"><h4><strong>Beruntung Bagi yang Sudah Vaksin Covid-19, Penelitian Ini Ungkap Perbedaan Kondisi Orang Sebelum dan Sesudah Vaksin Jik Terinfeksi Covid-19, Ternyata Begini Kondisi Darahnya....</strong></h4></a>
+                <!-- <p>Informasi tebaru mengatakan bahwa perusahaan X telah berhasil meluncurkan produk baru dengan teknologi GPS yang sangat canggih. Perusahaan X berhasil mengembangkan sebuah smartphone</p> -->
             </div>
         </div>
     </div>
