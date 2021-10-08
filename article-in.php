d64c471fe1026909b92dfd326e0c7117135620aa
rasya <rasyahroel132@gmail.com>
2021-10-08 15:46:51 +0700

    Revisi Article in

--- article-in.php
+++ article-in.php
@@ -1,73 +1,81 @@
 <?php include 'header.php' ?>
 
-<div class="col-9 ps-4 ms-4">
-	<h1>Judul Artikel</h1>
-</div>
-<div class="row ps-4 ms-4 p-4">
-	<div class="col-8">
-		<img src="image/kasus.jpg"  class="img-fluid img-medium ">
-		<div>
-			<p>Ini berisi isi Artikel</p>
+	<div class="row ps-4 ms-4 mt-5">
+		<h1><strong>Dampak Buruk Mie Instan</strong></h1>
+	</div>
+
+	<div class="row ps-4 pe-4 ms-4 mb-5">
+		<div class="col-8">
+			<img src="img/mie.jpg" alt="Mie Instan"  class="img-fluid img-medium w-100 py-5">
+			<div>
+				<p>Menurut sejumlah hasil penelitian, Terlalu sering mengkonsumsi mie instan dapat meningkatkan resiko timbulnya penyakit kanker, ginjal dan usus buntu. Pada sejumlah kasus, konsumsi mie instan berlebihan juga dapat menyebabkan kegemukan atau obesitas. <br><br>Apabila Anda biasanya mengkonsumsi mie instan setiap hari, mulailah untuk menguranginya dengan tenggang waktu 2hingga 3 hari dan lakukan sampai Anda terbebas dari mie instan sama sekali.Menurut penelitian ternyata di dalam mie instan terdapat kandungan lilin yang berbahaya kesehatan. Kandungan lilin dalam mie instan berguna untuk membuat mie tidak lengket satu dengan lainnya. Dengan seringnya kita mengkonsumsi mie instan berarti kita juga telah memasukkan banyak kandungan lilin ke dalam tubuh kita. Kandungan lilin tersebut akan merusak sistem kerja pencernaan dalam tubuh karena baru bisa dicerna oleh tubuh dalam waktuminimal 2 hari.<br><br>Selain kandungan lilin, di dalam mie instan juga terdapat natrium yang dapat menyebabkan penyakit tekanan darah tinggi (hipertensi) dan maag. Jika dikonsumsi berlebihan dalam waktu yang cukup lama, kandungan natrium ini tentu secara signifikan dapat menimbulkan penyakit di atas. Hal ini didasari oleh karena kandungan natrium yang bersifat menetralkan lambung, sehingga mengakibatkan lambung Anda akan mensekreasi asam dalam jumlah yang banyak agar dapat mencerna makanan. <br><br>Mie instan ternyata juga mempunyai kandungan zat-zat lain yang dapat membahayakan kesehatan tubuh kita, seperti MSG dan penambah rasa. Jika Anda masih ingin mengkonsumsi mie instan tetapi ingin tetap sehat Anda harus melakukannya sesuai saran penyajian, dan jangan memasak bumbu mie instan secara bersamaan mienya, karena jika bumbu mie instan dimasak di atas suhu120 oC bisa memicu terjadinya sel kanker.</p>
+			</div>
+		</div>
+
+		<div class="col-4 pe-4">
+			<div class="m-4">
+				<h2 class="text-center m-4"><strong>Popular<span class="text-yellow"> Article</span></strong></h2>
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>Dampak Buruk Mie Instan</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/mie.jpg" alt="Mie Instan" class="img-fluid">
+					</div>
+				</div>
+
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>Berhasilkah Pendidikan Kita?</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/pendidikan.jpg" alt="Pendidikan" class="img-fluid">
+					</div>
+				</div>
+
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>Teknologi GPS Terbaru</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/gps.jpg" alt="Teknologi GPS" class="img-fluid">
+					</div>
+				</div>
+
+				<a href="#" class="text-dark text-decoration-none"><h6 class="text-center m-4 mb-5"><strong>See more...</strong></h6></a>
+			</div>
+
+			<div class="m-4">
+				<h2 class="text-center m-4"><strong>Popular<span class="text-yellow"> News</span></strong></h2>
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>Pulang ke Manchester United, Cristiano Ronaldo Pakai Nomor Punggung Berapa?</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/mu.jpg" alt="Manchester United" class="img-fluid">
+					</div>
+				</div>
+
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>468 Kasus Baru Covid-19 di Jakarta Hari Ini, Positivity Rate di Bawah Batas Aman WHO</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/positivity-rate.jpg" alt="Positivity Rate" class="img-fluid">
+					</div>
+				</div>
+
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>Beruntung Bagi yang Sudah Vaksin Covid-19, Penelitian Ini Ungkap Perbedaan Kondisi Orang Sebelum dan Sesudah Vaksin Jik Terinfeksi Covid-19, Ternyata Begini Kondisi Darahnya....</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/blood-vaksin.jpg" alt="Vaksinasi" class="img-fluid">
+					</div>
+				</div>
+				<a href="#" class="text-dark text-decoration-none"><h6 class="text-center m-4"><strong>See more...</strong></h6></a>
+			</div>
 		</div>
 	</div>
-	<div class="col-4 pe-4">
-		<div class="m-4">
-			<h4 class="text-center m-4">Popular<span class="text-yellow"> article</span></h4>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<h6 class="text-center m-4">See more...</h6>
-		</div>
-		<div class="m-4">
-			<h4 class="text-center m-4">Popular<span class="text-yellow"> News</span></h4>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<h6 class="text-center m-4">See more...</h6>
-		</div>
-	</div>
-</div>
 
 <?php include 'footer.php' ?>
