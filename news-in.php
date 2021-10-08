044ee3f71d061a9c9390e7120282b2d8070d3100
rasya <rasyahroel132@gmail.com>
2021-10-08 16:01:05 +0700

    Revisi News in

--- news-in.php
+++ news-in.php
@@ -1,73 +1,84 @@
 <?php include 'header.php' ?>
 
-<div class="col-9 ps-4 ms-4">
-	<h1>Judul News</h1>
-</div>
-<div class="row ps-4 ms-4 p-4">
-	<div class="col-8">
-		<img src="image/kasus.jpg"  class="img-fluid img-medium ">
-		<div>
-			<p>Ini berisi isi News</p>
+	<div class="row ps-4 ms-4 mt-5">
+		<div class="col-8">
+			<h1><strong>Pulang ke Manchester United, Cristiano Ronaldo Pakai Nomor Punggung Berapa?</strong></h1>
+		</div>
+		
+	</div>
+
+	<div class="row ps-4 pe-4 ms-4 mb-5">
+		<div class="col-8">
+			<img src="img/mu.jpg" alt="Manchester United"  class="img-fluid img-medium w-100 py-5">
+			<div>
+				<p>Suara.com - Cristiano Ronaldo resmi kembali ke Manchester United, Jumat (27/8/2021) malam WIB. Lalu penyerang 36 tahun itu akan mengenakan nomor punggung berapa dalam periode keduanya merumput di Old Trafford? <br><br>Cristiano Ronaldo kembali ke Manchester United setelah 12 tahun berkelana bersama Real Madrid (2009-2018), dan Juventus (2018-2021). <br><br>Sayangnya, Cristiano Ronaldo pulang ke Old Trafford saat kompetisi Liga Inggris 2021/2022 sudah dimulai sejak 14 Agustus lalu. <br><br>Kondisi itu membuatnya tidak mungkin mengenakan nomor punggung 7 ikonik yang dia kenakan pada periode pertama di Manchester United. <br><br>Nomor punggung 7 untuk musim ini, telah didaftarkan atas nama penyerang Uruguay Edinson Cavani. Kemungkinan, CR7 baru bisa memakainya di musim depan. <br><br>Melansir The Guardian, Jumat (27/8/2021), Setan Merah sudah mengajukan tawaran resmi untuk mengontrak peraih lima Ballon d'Or itu selama dua musim hingga 2023. <br><br>Tim asuhan Ole Gunnar Solskjaer itu menyodorkan 25 juta euro plus bonus untuk Juventus yang diklaim telah disepakati kedua belah pihak. <br><br>Ronaldo sebagaimana laporan Goal, telah meninggalkan Turin dengan pesawat pribadi. Penyerang sekaligus kapten Timnas Portugal itu diyakini tengah menuju kota Manchester. <br><br>Sebelumnya, kabar Cristiano Ronaldo ingin meninggalkan Juventus telah dikonfirmasi pelatih Bianconeri, Massimiliano Allegri. Dia tak menampik CR7 ingin hengkang dari Turin.</p>
+			</div>
+		</div>
+
+		<div class="col-4 pe-4">
+			<div class="m-4">
+				<h2 class="text-center m-4"><strong>Popular<span class="text-yellow"> Articles</span></strong></h2>
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>Dampak Buruk Mie Instan</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/mie.jpg" alt="Mie Instan" class="img-fluid">
+					</div>
+				</div>
+
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>Berhasilkah Pendidikan Kita?</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/pendidikan.jpg" alt="Pendidikan" class="img-fluid">
+					</div>
+				</div>
+
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>Teknologi GPS Terbaru</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/gps.jpg" alt="Teknologi GPS" class="img-fluid">
+					</div>
+				</div>
+
+				<a href="#" class="text-dark text-decoration-none"><h6 class="text-center m-4 mb-5"><strong>See more...</strong></h6></a>
+			</div>
+
+			<div class="m-4">
+				<h2 class="text-center m-4"><strong>Popular<span class="text-yellow"> News</span></strong></h2>
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>Pulang ke Manchester United, Cristiano Ronaldo Pakai Nomor Punggung Berapa?</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/mu.jpg" alt="Manchester United" class="img-fluid">
+					</div>
+				</div>
+
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>468 Kasus Baru Covid-19 di Jakarta Hari Ini, Positivity Rate di Bawah Batas Aman WHO</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/positivity-rate.jpg" alt="Positivity Rate" class="img-fluid">
+					</div>
+				</div>
+
+				<div class="row my-2">
+					<div class="col-7">
+						<a href="#" class="text-dark text-decoration-none"><h6><strong>Beruntung Bagi yang Sudah Vaksin Covid-19, Penelitian Ini Ungkap Perbedaan Kondisi Orang Sebelum dan Sesudah Vaksin Jik Terinfeksi Covid-19, Ternyata Begini Kondisi Darahnya....</strong></h6></a>
+					</div>
+					<div class="col-5">
+						<img src="img/blood-vaksin.jpg" alt="Vaksinasi" class="img-fluid">
+					</div>
+				</div>
+				<a href="#" class="text-dark text-decoration-none"><h6 class="text-center m-4"><strong>See more...</strong></h6></a>
+			</div>
 		</div>
 	</div>
-	<div class="col-4 pe-4">
-		<div class="m-4">
-			<h4 class="text-center m-4">Popular<span class="text-yellow"> article</span></h4>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<h6 class="text-center m-4">See more...</h6>
-		</div>
-		<div class="m-4">
-			<h4 class="text-center m-4">Popular<span class="text-yellow"> News</span></h4>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<div class="row my-2">
-				<div class="col-7">
-					<h6>Ini judul berita</h6>
-				</div>
-				<div class="col-5">
-					<img src="image/kasus.jpg" class="img-fluid">
-				</div>
-			</div>
-			<h6 class="text-center m-4">See more...</h6>
-		</div>
-	</div>
-</div>
 
 <?php include 'footer.php' ?>
