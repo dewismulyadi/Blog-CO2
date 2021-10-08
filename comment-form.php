1d019326c28837b096b29e1c7785d2b3a158628e
rasya <rasyahroel132@gmail.com>
2021-10-08 16:57:48 +0700

    Revisi form

--- comment-form.php
+++ comment-form.php
@@ -3,38 +3,42 @@
 <div class="w-100 h-100 bg-yellowalt p-4">
     <div class="bg-white m-4 p-4 rounded-alt">
         <div class="text-center my-4 py-4">
-            <h1>Send<span class="text-yellow"> Comment</span></h1>
+            <h1><strong>Send<span class="text-yellow"> Comment</span></strong></h1>
         </div>
-        <div class="m-4 p-4">
-            <div class="row px-4 py-3">
-                <div class="col-3">
-                    <p>Name</p>
+
+        <div class="m-auto p-auto">
+            <div class="row px-5 py-3">
+                <div class="col-md-3 col-sm-12">
+                    <h4 class="">Name</h4>
                 </div>
-                <div class="row col-9">
+                <div class="col-md-9 col-sm-12">
                     <input type="name" placeholder="Input your name" class="form-text border-0 border-bottom w-100" id="disabledTextInput" aria-describedby="emailHelp">
                 </div>
             </div>
-            <div class="row px-4 py-3">
-                <div class="col-3">
-                    <p>Email</p>
+
+            <div class="row px-5 py-3">
+                <div class="col-md-3 col-sm-12">
+                    <h4>Email</h4>
                 </div>
-                <div class="col-9">
-                    <input type="email" placeholder="Input your email" class="border-0 border-bottom w-100" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div class="col-md-9 col-sm-12">
+                    <input type="email" placeholder="Input your email" class="form-text border-0 border-bottom w-100" id="exampleInputEmail1" aria-describedby="emailHelp">
                 </div>
             </div>
-            <div class="row px-4 py-3">
-                <div class="col-3">
-                    <p>Comment</p>
+
+            <div class="row px-5 py-3">
+                <div class="col-md-3 col-sm-12">
+                    <h4>Comment</h4>
                 </div>
-                <div class="col-9">
-                    <input type="comment" placeholder="Input your comment" class="border-0 border-bottom w-100" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div class="my-4 py-3">
-                        <button class="bg-darkalt text-yellow rounded-alt px-4">SEND</button>
+                <div class="col-md-9 col-sm-12">
+                    <input type="comment" placeholder="Input your comment" class="form-text border-0 border-bottom w-100" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div class="col-2 my-4 py-3">
+                        <button class="bg-darkalt text-yellow rounded-alt px-5 py-2">SEND</button>
                     </div>
                 </div>
             </div>
-            
         </div>
+
+        <p class="text-center mt-5">For any other inquiries please contact <a class="text-decoration-none" href="#">support@codingcollective.com</a></p>
     </div>
 </div>
 
